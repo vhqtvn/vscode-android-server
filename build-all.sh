94e79b4c554856a2bd65e8a09de8d78488a5303e
vh <vh@polaris>
2020-06-11 13:09:17 +0000

    exit when build error

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 BUILD_NODE=1 BUILD_RELEASE=1 ANDROID_ARCH=aarch64 ./dev.sh release
 BUILD_NODE=1 BUILD_RELEASE=1 ANDROID_ARCH=arm ./dev.sh release
 BUILD_NODE=1 BUILD_RELEASE=1 ANDROID_ARCH=x86_64 ./dev.sh release
