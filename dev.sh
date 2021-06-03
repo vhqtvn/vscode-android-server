d87feeae5db1bfbfbe7f28d14daf13a1b452537a
vhnvn <vh@fake.com>
2021-06-04 01:06:54 +0700

    update code-server to 3.10.2

--- dev.sh
+++ dev.sh
@@ -13,8 +13,12 @@ main() {
         diff -x node_modules -x build -x .build -x 'out-*' -x out -ru vscode.orig vscode > ../ci/dev/vscode.vh.patch
         ;;
       apply-patch)
-        cd code-server/lib/vscode
-        git apply ../../../vscode.vh.patch
+        if [[ -f ./vscode.vh.patch ]]; then
+          cd code-server/lib/vscode && git apply ../../../vscode.vh.patch
+        fi
+        if [[ -f ./node-src.vh.patch ]]; then
+          cd node-src && git apply ../node-src.vh.patch
+        fi
         ;;
       build-android-env)
         docker build ./container/android -t vsandroidenv:latest
@@ -45,30 +49,33 @@ main() {
 	set -e
         if [ ! -z "$BUILD_NODE" ]; then
           cd node-src
-          rm -rf out || true
+          rm -rf out
           make clean || true
           PATH=/vscode-build/hostbin:$PATH CC_host=gcc CXX_host=g++ LINK_host=g++ ./android-configure /opt/android-ndk/ $NODE_CONFIGURE_NAME $ANDROID_BUILD_API_VERSION
           PATH=/vscode-build/hostbin:$PATH JOBS=$(nproc) make -j $(nproc)
           cd ..
         fi
         if [ ! -z "$BUILD_RELEASE" ]; then
-          cd code-server
-          rm -rf release release-static node_modules || true
-          CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn
-          cd lib/vscode && (rm -rf node_modules || true) && CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn
-          cd ../..
-          CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn build
-          CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn build:vscode
-          CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn release
-          CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn release:static
-          cd release-static
-          CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn --production --frozen-lockfile
-          cd ../..
+          pushd code-server
+            rm -rf release release-standalone node_modules
+            CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn
+            pushd lib/vscode
+              rm -rf node_modules
+              CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn
+            popd
+            CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn build
+            CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn build:vscode
+            CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn release
+            CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn release:standalone
+            cd release-standalone
+            CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn --production --frozen-lockfile
+          popd
         fi
         rm -rf cs-$ANDROID_ARCH.tgz libc++_shared.so node
         cp node-src/out/Release/node ./
         cp /opt/android-ndk/sources/cxx-stl/llvm-libc++/libs/$ARCH_NAME/libc++_shared.so ./libc++_shared.so
-        tar -czvf cs-$ANDROID_ARCH.tgz code-server/release-static code-server/VERSION node "libc++_shared.so"
+	cat code-server/package.json | jq -r '.version' > code-server/VERSION
+        tar -czvf cs-$ANDROID_ARCH.tgz code-server/release-standalone code-server/VERSION node "libc++_shared.so"
         ;;
       *)
         docker run --rm -it \
