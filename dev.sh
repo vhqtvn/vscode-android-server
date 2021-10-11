7d529b9097efcae022ab133c25cbeb26be03abce
vhnvn <vh@fake.com>
2021-10-11 21:07:21 +0700

    update to code-server 3.12.0

--- dev.sh
+++ dev.sh
@@ -57,7 +57,7 @@ main() {
           cd node-src
           rm -rf out
           make clean
-          if [[ "$ANDROID_ARCH" == "x86_64" ]] || []; then
+          if [[ "$ANDROID_ARCH" == "x86_64" ]]; then
             git checkout HEAD -- ./deps/v8/src/api/api.cc
             mv ./deps/v8/src/api/api.cc ./deps/v8/src/api/api.cc.orig
             cat ./deps/v8/src/api/api.cc.orig | sed 's/#if V8_TARGET_ARCH_X64 && !V8_OS_ANDROID/#if true\nreturn false;\n#elif false/g' > ./deps/v8/src/api/api.cc
@@ -93,10 +93,17 @@ main() {
           pushd code-server
             rm -rf release release-standalone node_modules
             CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn
-            pushd lib/vscode
-              rm -rf node_modules
-              CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn
-            popd
+            find . -iname yarn.lock | grep -v node_modules | while IPS= read -l dir
+            do
+              echo "$dir"
+              pushd "$(basename "$dir")"
+                CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn
+                [[ "$(jq ".scripts.build" code-server/package.json )" != "null" ]] && CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn build
+                [[ "$(jq ".scripts.release" code-server/package.json )" != "null" ]] && CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn release
+                [[ "$(jq ".scripts[\"release:standalone\"]" code-server/package.json )" != "null" ]] && CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn release:standalone
+                CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn --production --frozen-lockfile
+              popd
+            done
             CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn build
             CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn build:vscode
             CC_target=cc AR_target=ar CXX_target=cxx LINK_target=ld PATH=/vscode-build/bin:$PATH yarn release
@@ -111,7 +118,7 @@ main() {
         cp /opt/android-ndk/sources/cxx-stl/llvm-libc++/libs/$ARCH_NAME/libc++_shared.so ./libc++_shared.so
       	cat code-server/package.json | jq -r '.version' > code-server/VERSION
         ANDROID_ARCH=$ANDROID_ARCH TERMUX_ARCH=$TERMUX_ARCH bash ./scripts/download-rg.sh
-        cp rg/$ANDROID_ARCH/rg code-server/release-standalone/lib/vscode/node_modules/vscode-ripgrep/bin/rg
+        cp rg/$ANDROID_ARCH/rg code-server/release-standalone/vendor/modules/code-oss-dev/node_modules/vscode-ripgrep/bin/rg
         echo md5 code-server/release-standalone/lib/vscode/node_modules/vscode-ripgrep/bin/rg
         if [[ "$(find code-server/release-standalone -iname '*.orig')" != "" ]]; then
           find code-server/release-standalone -iname '*.orig'
--- scripts/download-rg.sh
+++ scripts/download-rg.sh
@@ -3,11 +3,11 @@ mkdir -p rg
 rm -rf rg/$ANDROID_ARCH || true
 mkdir -p rg/$ANDROID_ARCH
 deb_uri=
-curl "https://vsc.vhn.vn/termux-packages-24/dists/stable/main/binary-$TERMUX_ARCH/Packages" \
+curl -k "https://vsc.vhn.vn/termux-packages-24/dists/stable/main/binary-$TERMUX_ARCH/Packages" \
     | grep Filename \
     | grep '/ripgrep_' \
     | awk '{print $2}'
-curl "https://vsc.vhn.vn/termux-packages-24/dists/stable/main/binary-$TERMUX_ARCH/Packages" \
+curl -k "https://vsc.vhn.vn/termux-packages-24/dists/stable/main/binary-$TERMUX_ARCH/Packages" \
     | grep Filename \
     | grep '/ripgrep_' \
     | awk '{print $2}' \
@@ -19,7 +19,7 @@ curl "https://vsc.vhn.vn/termux-packages-24/dists/stable/main/binary-$TERMUX_ARC
             exit -1
         fi
         pushd rg/$ANDROID_ARCH
-            wget https://vsc.vhn.vn/termux-packages-24/$deb_uri -O rg.deb
+            wget --no-check-certificate https://vsc.vhn.vn/termux-packages-24/$deb_uri -O rg.deb
             ar x rg.deb data.tar.xz
             tar xf data.tar.xz
             mv data/data/vn.vhn.vsc/files/usr/bin/rg rg
