30a450002db816982da6ae9b2a74916f14c47ae7
vhnvn <vh@fake.com>
2022-02-27 22:26:32 +0700

    update

---
+++ argon/index.js
@@ -0,0 +1,6 @@
+const { Algorithm, Version, hash, verify } = require('./argon2.node')
+
+module.exports.Algorithm = Algorithm
+module.exports.Version = Version
+module.exports.hash = hash
+module.exports.verify = verify
